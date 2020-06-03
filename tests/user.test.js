1b4f3ad8eddbac06a798bfb5536ff59892cddd11
hpc <philipchanhk626@gmail.com>
2020-06-03 11:42:14 -0400

    added seed test data in test db

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,9 +1,46 @@
 import 'cross-fetch/polyfill'
 import ApolloBoost, { gql } from 'apollo-boost'
+import bcrypt from 'bcryptjs'
 import prisma from '../src/prisma'
 
 const client = new ApolloBoost({
   uri: 'http://localhost:4000'
+})
+
+beforeEach(async () => {
+  await prisma.mutation.deleteManyPosts()
+  await prisma.mutation.deleteManyUsers()
+  const user = await prisma.mutation.createUser({
+    data: {
+      name: 'Jen',
+      email: 'jen@example.com',
+      password: bcrypt.hashSync('red12345')
+    }
+  })
+  await prisma.mutation.createPost({
+    data: {
+      title: 'Test Published Post',
+      body: '',
+      published: true,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  })
+  await prisma.mutation.createPost({
+    data: {
+      title: 'Test Draft Post',
+      body: '',
+      published: false,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  })
 })
 
 test('Should create a new user', async () => {
